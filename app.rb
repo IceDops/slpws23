ee2e008148e57d10238602d20afb802394f1f536
IceDops <johannesnydahl1@gmail.com>
2023-02-23 11:45:49 +0100

    Kan nu hämta all information om ett viss medie

--- app.rb
+++ app.rb
@@ -31,24 +31,19 @@ end
 
 get("/media/:media_id") do
  
-
-    medias = db.execute("SELECT * FROM Media")
-
-    sought_media = nil
     sought_id = params[:media_id].to_i
 
-    medias.each do | media |
-        if media["id"] == sought_id 
-            sought_media = media 
-        end
-    end
+    sought_media = db.execute("SELECT * FROM Media WHERE Media.id = ?", sought_id)
 
-    if sought_media == nil 
+    if sought_media.empty?
        return slim(:"error", locals:{document_title: "404", error_message: "The media specified is not found."})
     end
 
-    slim(:"media", locals:{document_title: sought_media["name"], media: sought_media})
-   end
+    genres = db.execute("SELECT Genre.name FROM Genre INNER JOIN Media_genre_relation ON Genre.id = Media_genre_relation.genre_id WHERE Media_genre_relation.media_id = ?;", sought_id)
+    authors = db.execute("SELECT Author.name FROM Author INNER JOIN Media_author_relation ON Author.id = Media_author_relation.author_id WHERE Media_author_relation.media_id = ?", sought_id)
+   
+    slim(:"media", locals:{document_title: sought_media[0]["name"], media: sought_media[0], genres: genres, authors: authors })
+end
 
 get("/admin") do
 
