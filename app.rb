b9cc6afbf58fad3c1d3886aa24714813b7e021dd
IceDops <johannesnydahl1@gmail.com>
2023-03-18 00:12:37 +0100

    Yardoc och REST samt Prettier

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+# TODO
+# Lägg mer av logiken i model.rb. Kolla t.ex inte ifall en recension existerar i app.rb
+
 require "sinatra"
 require "slim"
 require "sqlite3"
@@ -8,82 +11,187 @@ require_relative "./model.rb"
 
 enable :sessions
 
-db = database("./db/main.db")
+include Model
 
-get('/') do 
+db = database("./db/main.db")
+# YARDOC ROUTE TEMPLATE
+# Deletes an existing article and redirects to '/articles'
+#
+# @param [Integer] :id, The ID of the article
+# @param [String] title, The new title of the article
+# @param [String] content, The new content of the article
+#
+# @see Model#delete_article
+
+get("/") do
     # Replace 1 with user_id
     followings_reviews = followings_reviews(db, 1)
-    slim(:"index", locals:{document_title: "Hem", followings_reviews: followings_reviews, db: db})
+    slim(:"index", locals: { document_title: "Hem", followings_reviews: followings_reviews, db: db })
 end
 
-get("/signup") do
+get("/signup") {}
 
+get("/search") {}
+
+# Putting this before /review/:review_id so it matches this first, otherwise it will think that new is an id
+get("/media/new") { return(slim(:"review/new", locals: { document_title: "Publisera en recension" })) }
+
+# Finds all reviews belonging to a media
+#
+# @param [Integer] :media_id, The ID of the media
+#
+# @see Model#get_all_reviews
+get("/medias/:media_id/reviews") do
+    media_id = params[:media_id].to_i
+    reviews = get_all_reviews(db, media_id)
+    slim(:"review/index", locals: { document_title: "Reviews", reviews: reviews, db: db })
 end
 
-get("/search") do
+# Finds a specific review to a certain media
+#
+# @param [Integer] :media_id, The ID of the media
+# @param [Integer] :review_id, The ID of the review
+#
+# @see Model#review
+get("/medias/:media_id/reviews/:review_id") do
+    media_id = params[:media_id].to_i
+    review_id = params[:review_id].to_i
 
-end
+    sought_review = review(db, review_id)
 
-get("/review") do
-    reviews = get_all_reviews(db)
-    slim(:"review/index", locals:{document_title: "Reviews", reviews: reviews, db: db})
-end
-
-# Putting this before /review/:review_id so it matches this first, otherwise it will think that new is an id 
-get("/review/new") do
-    return slim(:"review/new", locals:{document_title: "Publisera en recension"})
-end
-
-get("/review/:review_id/edit") do
-    sought_id = params[:review_id].to_i
-    review = review(db, sought_id)
-
-    return slim(:"review/edit", locals:{document_title: "Redigera en recension", review: review})
-end
-
-
-get("/review/:review_id") do
-
-    sought_id = params[:review_id].to_i
-    sought_review = review(db, sought_id)
-
-
-    if sought_review.empty?
-       return slim(:"error", locals:{document_title: "404", error_message: "The review specified is not found."})
+    if sought_review.empty? || sought_review["media_id"] != media_id
+        return(
+            slim(:"error", locals: { document_title: "404", error_message: "The review specified is not found." })
+        )
     end
 
     media_name = media_id_to_name(db, sought_review["media_id"])
-    print(sought_review["user_id"])
 
-    slim(:"review/show", locals:{document_title: "Review: #{media_name}", review: sought_review, 
-        media_name: media_name, user_author_name: user_ids_to_names(db, [sought_review["user_id"]]), date: Time.at(sought_review["creation_date"]).to_date
-    })
+    slim(
+        :"review/show",
+        locals: {
+            document_title: "Review: #{media_name}",
+            review: sought_review,
+            media_name: media_name,
+            user_author_name: user_ids_to_names(db, [sought_review["user_id"]]),
+            date: Time.at(sought_review["creation_date"]).to_date
+        }
+    )
 end
 
-get("/user/:user_id") do
+# Displays the edit menu for a certain review
+#
+# @param [Integer] :media_id, The ID of the media
+# @param [Integer] :review_id, The ID of the review
+#
+# @see Model#review
+get("/medias/:media_id/reviews/:review_id/edit") do
+    media_id = params[:media_id].to_i
+    review_id = params[:review_id].to_i
+
+    sought_review = review(db, review_id)
+
+    if sought_review.empty? || sought_review["media_id"] != media_id
+        return(
+            slim(:"error", locals: { document_title: "404", error_message: "The review specified is not found." })
+        )
+    end
+
+    return(
+        slim(
+            :"review/edit",
+            locals: {
+                document_title: "Redigera en recension",
+                review: sought_review,
+                media_id: media_id
+            }
+        )
+    )
+end
+
+# Updates a certain review in the specified database
+# @param [SQLite3::Database] db, The database where the review and the media is stored
+# @param [Integer] :media_id, The ID of the media
+# @param [Integer] :review_id, The ID of the review
+#
+# @see Model#update_review
+post("/medias/:media_id/reviews/:review_id/edit") do
+    media_id = params[:media_id].to_i
+    review_id = params[:review_id].to_i
+
+    if !update_review(
+              db,
+              media_id,
+              review_id,
+              { edited_review_desc: params[:review_desc], edited_review_rating: params[:review_rating] }
+          )
+        return(
+            slim(:"error", locals: { document_title: "404", error_message: "The review specified is not found." })
+        )
+    end
+
+    redirect("/medias/#{media_id}/reviews/#{review_id}")
+end
+
+# Deletes an review and redirects to /review
+#
+# @param [Integer] :media_id, The ID of the media
+# @param [Integer] :review_id, The ID of the to be deleted review
+#
+# @see Model#delete_review
+get("/medias/:media_id/review/:review_id/delete") do
+    media_id = params[:media_id].to_i
+    review_id = params[:review_id].to_i
+
+    if !delete_review(db, media_id, review_id)
+        return(
+            slim(:"error", locals: { document_title: "404", error_message: "The review specified is not found." })
+        )
+    end
+
+    redirect("/review")
+end
+
+get("/users/:user_id") do
     sought_id = params[:user_id].to_i
     sought_user = users(db, [sought_id])[0]
 
     if sought_user.empty?
-       return slim(:"error", locals:{document_title: "404", error_message: "The the user specified is not found."})
+        return(
+            slim(:"error", locals: { document_title: "404", error_message: "The the user specified is not found." })
+        )
     end
 
-    slim(:"user", locals: {document_title: sought_user["name"],
-        user: sought_user, following_names: user_ids_to_names(db, sought_user[:following_ids]), 
-        follower_names: user_ids_to_names(db, sought_user[:follower_ids]), date: Time.at(sought_user["creation_date"]).to_date
-    }) 
+    slim(
+        :"user",
+        locals: {
+            document_title: sought_user["name"],
+            user: sought_user,
+            following_names: user_ids_to_names(db, sought_user[:following_ids]),
+            follower_names: user_ids_to_names(db, sought_user[:follower_ids]),
+            date: Time.at(sought_user["creation_date"]).to_date
+        }
+    )
 end
 
-get("/media/:media_id") do
+get("/medias/:media_id") do
     sought_id = params[:media_id].to_i
-    sought_media = media(db, sought_id) 
+    sought_media = media(db, sought_id)
 
     if sought_media.empty?
-       return slim(:"error", locals:{document_title: "404", error_message: "The media specified is not found."})
+        return(
+            slim(:"error", locals: { document_title: "404", error_message: "The media specified is not found." })
+        )
     end
-     
-    slim(:"media", locals:{document_title: sought_media["name"], 
-        media: sought_media, author_names: author_ids_to_names(db, sought_media[:author_ids]), 
-        genre_names: genre_ids_to_names(db, sought_media[:genre_ids]), date: Time.at(sought_media["creation_date"]).to_date
-    })
+
+    slim(
+        :"media",
+        locals: {
+            document_title: sought_media["name"],
+            media: sought_media,
+            author_names: author_ids_to_names(db, sought_media[:author_ids]),
+            genre_names: genre_ids_to_names(db, sought_media[:genre_ids]),
+            date: Time.at(sought_media["creation_date"]).to_date
+        }
+    )
 end
--- model.rb
+++ model.rb
@@ -1,122 +1,196 @@
 # TODO
-# Använd "helpers" mer 
+# Använd "helpers" mer
 
-def database(path) 
-    db = SQLite3::Database.new('db/main.db')
-	db.results_as_hash = true
-    return db
-end
-
+# YARDOC DOCUMENTATION TEMPLATE FOR FUNCTIONS
+# Attempts to insert a new row in the articles table
+#
+# @param [Hash] params form data
+# @option params [String] title The title of the article
+# @option params [String] content The content of the article
+#
+# @return [Hash]
+#   * :error [Boolean] whether an error occured
+#   * :message [String] the error message
 helpers do
-    def user_ids_to_names(db, user_ids) 
-        return users(db, user_ids).map {|user| user["name"]}
+    def user_ids_to_names(db, user_ids)
+        return users(db, user_ids).map { |user| user["name"] }
     end
 
-    def media_id_to_name(db, media_id) 
+    def media_id_to_name(db, media_id)
         return db.execute("SELECT Media.name FROM Media WHERE Media.id = ?;", media_id)[0]["name"]
     end
 
-    def format_unix_timestamp(timestamp) 
+    def format_unix_timestamp(timestamp)
         return Time.at(timestamp).to_date
     end
 end
 
-def users(db, user_ids)
-    users = []
-    user_ids.each do | user_id |
-        sought_user = db.execute("SELECT * FROM User WHERE User.id = ?", user_id)
-        if sought_user.empty?
-            next
+module Model
+    def database(path)
+        db = SQLite3::Database.new("db/main.db")
+        db.results_as_hash = true
+        return db
+    end
+
+    # Checks if review ID exist and belongs belongs to a certain media in the database
+    #
+    # @param [SQLite3::Database] database where review is stored
+    # @param [Number] the media's id in the database
+    # @param [Number] the review's id in the database
+    #
+    # @return [Boolean] if it exist
+    def does_review_exist(db, media_id, review_id)
+        review = review(db, review_id)
+        if review(db, review_id).empty? || media_id = !review["media_id"]
+            return false
+        else
+            return true
+        end
+    end
+
+    def users(db, user_ids)
+        users = []
+        user_ids.each do |user_id|
+            sought_user = db.execute("SELECT * FROM User WHERE User.id = ?", user_id)
+            next if sought_user.empty?
+
+            followings =
+                db.execute(
+                    "SELECT User.id FROM User INNER JOIN User_follow_relation ON User.id = User_follow_relation.user_follows_this_id WHERE User_follow_relation.user_id = ?;",
+                    user_id
+                )
+            followers =
+                db.execute(
+                    "SELECT User.id FROM User INNER JOIN User_follow_relation ON User.id = User_follow_relation.user_id WHERE User_follow_relation.user_follows_this_id = ?;",
+                    user_id
+                )
+
+            sought_user_complete = sought_user[0]
+            # .map is syntactic sugar for extracting each id property out of the array of following/follower hashes
+            sought_user_complete[:following_ids] = followings.map { |following| following["id"] }
+            sought_user_complete[:follower_ids] = followers.map { |follower| follower["id"] }
+            users.push(sought_user_complete)
         end
 
-        followings = db.execute("SELECT User.id FROM User INNER JOIN User_follow_relation ON User.id = User_follow_relation.user_follows_this_id WHERE User_follow_relation.user_id = ?;", user_id) 
-        followers = db.execute("SELECT User.id FROM User INNER JOIN User_follow_relation ON User.id = User_follow_relation.user_id WHERE User_follow_relation.user_follows_this_id = ?;", user_id)
-
-        sought_user_complete = sought_user[0]
-        # .map is syntactic sugar for extracting each id property out of the array of following/follower hashes
-        sought_user_complete[:following_ids] = followings.map { |following| following["id"] }
-        sought_user_complete[:follower_ids] = followers.map { |follower| follower["id"] }
-        users.push(sought_user_complete)  
-    end
-    
-    return users
-end
-
-def media(db, media_id)
-    sought_media = db.execute("SELECT * FROM Media WHERE Media.id = ?", media_id)
-    if sought_media.empty?
-        return []
+        return users
     end
 
-    genres = db.execute("SELECT Genre.id FROM Genre INNER JOIN Media_genre_relation ON Genre.id = Media_genre_relation.genre_id WHERE Media_genre_relation.media_id = ?;", media_id)
-    authors = db.execute("SELECT Author.id FROM Author INNER JOIN Media_author_relation ON Author.id = Media_author_relation.author_id WHERE Media_author_relation.media_id = ?;", media_id)
+    def media(db, media_id)
+        sought_media = db.execute("SELECT * FROM Media WHERE Media.id = ?", media_id)
+        return [] if sought_media.empty?
 
-    sought_media_return = sought_media[0]
-    # .map is syntactic sugar for extracting each id property out of the array of genre/author hashes
-    sought_media_return[:genre_ids] = genres.map { |genre| genre["id"] } 
-    sought_media_return[:author_ids] = authors.map { |author| author["id"]}
-    return sought_media_return
-end
+        genres =
+            db.execute(
+                "SELECT Genre.id FROM Genre INNER JOIN Media_genre_relation ON Genre.id = Media_genre_relation.genre_id WHERE Media_genre_relation.media_id = ?;",
+                media_id
+            )
+        authors =
+            db.execute(
+                "SELECT Author.id FROM Author INNER JOIN Media_author_relation ON Author.id = Media_author_relation.author_id WHERE Media_author_relation.media_id = ?;",
+                media_id
+            )
 
-def review(db, review_id)
-    sought_review = db.execute("SELECT * FROM Review WHERE Review.id = ?", review_id)
-    if sought_review.empty?
-        return []
-    end
-    return sought_review[0]
-end
-
-def get_all_reviews(db)
-    reviews = db.execute("SELECT * FROM Review")
-    if reviews.empty?
-        return []
+        sought_media_return = sought_media[0]
+        # .map is syntactic sugar for extracting each id property out of the array of genre/author hashes
+        sought_media_return[:genre_ids] = genres.map { |genre| genre["id"] }
+        sought_media_return[:author_ids] = authors.map { |author| author["id"] }
+        return sought_media_return
     end
 
-    reviews = reviews.sort_by {|review| review["creation_date"] *-1 }
-
-    return reviews
-end
-
-def user_reviews(db, user_id)
-    print(user_id)
-    reviews = db.execute("SELECT * FROM Review WHERE Review.user_id = ?;", user_id)
-    return reviews
-end
-
-
-def followings_reviews(db, user_id) 
-    followings = users(db, [user_id])[0][:following_ids]
-
-    #print("Followings: ")
-    #print(followings)
-
-
-    followings_reviews = []
-    followings.each do |following|
-        followings_reviews = followings_reviews + user_reviews(db, following)
+    def review(db, review_id)
+        sought_review = db.execute("SELECT * FROM Review WHERE Review.id = ?", review_id)
+        return [] if sought_review.empty?
+        return sought_review[0]
     end
 
-    followings_reviews = followings_reviews.sort_by {|review| review["creation_date"] *-1 }
+    def get_all_reviews(db, media_id)
+        reviews = db.execute("SELECT * FROM Review WHERE Review.media_id = ?", media_id)
+        return [] if reviews.empty?
 
-    return followings_reviews
-end
+        reviews = reviews.sort_by { |review| review["creation_date"] * -1 }
 
-
-def author_ids_to_names(db, author_ids) 
-    author_names = [];
-    print(author_ids)
-    author_ids.each do | author_id |
-        author_names.push(db.execute("SELECT Author.name FROM Author WHERE Author.id = ?;", author_id)[0]["name"])
+        return reviews
     end
-    return author_names
-end
 
-def genre_ids_to_names(db, genre_ids) 
-    genre_names = [];
-    genre_ids.each do | genre_id |
-        genre_names.push(db.execute("SELECT Genre.name FROM Genre WHERE Genre.id = ?;", genre_id)[0]["name"])
+    def user_reviews(db, user_id)
+        print(user_id)
+        reviews = db.execute("SELECT * FROM Review WHERE Review.user_id = ?;", user_id)
+        return reviews
     end
-    return genre_names
+
+    def followings_reviews(db, user_id)
+        followings = users(db, [user_id])[0][:following_ids]
+
+        #print("Followings: ")
+        #print(followings)
+
+        followings_reviews = []
+        followings.each { |following| followings_reviews = followings_reviews + user_reviews(db, following) }
+
+        followings_reviews = followings_reviews.sort_by { |review| review["creation_date"] * -1 }
+
+        return followings_reviews
+    end
+
+    def author_ids_to_names(db, author_ids)
+        author_names = []
+        print(author_ids)
+        author_ids.each do |author_id|
+            author_names.push(
+                db.execute("SELECT Author.name FROM Author WHERE Author.id = ?;", author_id)[0]["name"]
+            )
+        end
+        return author_names
+    end
+
+    def genre_ids_to_names(db, genre_ids)
+        genre_names = []
+        genre_ids.each do |genre_id|
+            genre_names.push(db.execute("SELECT Genre.name FROM Genre WHERE Genre.id = ?;", genre_id)[0]["name"])
+        end
+        return genre_names
+    end
+
+    # Updates the specified review in the database provided with provided information
+    #
+    # @param [SQLite3::Database] database where review is stored
+    # @param [Number] the the media id belonging to the review in the database
+    # @param [Number] the review's id in the database
+    # @param [Hash] the update review
+    #   * :edited_review_rating [Number] the new review rating
+    #   * :edited_review_desc [String] the new review description
+    #
+    # @return [Boolean] was it updated?
+    def update_review(db, media_id, review_id, updated_review)
+        if does_review_exist(db, media_id, review_id)
+            db.execute(
+                "UPDATE Review
+                SET content = ?, rating = ?, creation_date = ? 
+                WHERE id = ?; ",
+                updated_review[:edited_review_desc],
+                updated_review[:edited_review_rating],
+                Time.now.to_i,
+                review_id
+            )
+            return true
+        else
+            return false
+        end
+    end
 end
 
-
+# Deletes specifed review in the database provided
+#
+# @param [SQLite3::Database] database where review is stored
+# @param [Number] the the media id belonging to the review in the database
+# @param [Number] the review's id in the database
+#
+# @return [Boolean] was it deleted?
+def delete_review(db, media_id, review_id)
+    if does_review_exist(db, media_id, review_id)
+        db.execute("DELETE FROM Review WHERE id = ?", review_id)
+        return true
+    else
+        return false
+    end
+end
